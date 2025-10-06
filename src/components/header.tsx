ce81748ebc7b96d4049fe0110503525ba948b2b2
Rana <rana78085@gmail.comm>
2025-10-06 08:33:05 +0600

    build

---
+++ src/components/header.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+const Navigation: React.FC = () => {
+  const [activeMenu, setActiveMenu] = useState(false);
+  const mobileMenuToggle = (): void => {
+    setActiveMenu(!activeMenu);
+  };
+
+  
+
+  return (
+    <nav className={`navigations ${activeMenu ? "bg-black" : "pe-none-m"}`}>
+      <ul className="menu-for-desktop">
+        <li>
+          <a href="/">
+            <img src="/images/brainvector_logo.svg" alt="Brainvector logo" />
+          </a>
+        </li>
+        <li>
+          <a href="/">How it works</a>
+        </li>
+        <li>
+          <a href="#">Explore our solutions</a>
+        </li>
+        <li>
+          <a href="/backed-by-research">Backed by research</a>
+        </li>
+        <li>
+          <a href="/about-us">About Us</a>
+        </li>
+      </ul>
+      <a href="#" className="book-a-demo">
+        Book a demo
+      </a>
+      <div className="menu-for-mobile">
+        <div className="pe-auto">
+          <a href="/">
+            <img src="/images/brainvector_logo.svg" alt="Brainvector logo" />
+          </a>
+          <button onClick={mobileMenuToggle} aria-label="Toggle mobile menu">
+            {activeMenu ? (
+              <img className="bars" src="/images/close.svg" alt="bars" />
+            ) : (
+              <img className="bars" src="/images/bars.svg" alt="bars" />
+            )}
+          </button>
+        </div>
+
+        <div
+          className={`mobile-menu-items ${
+            activeMenu ? "mobile-menu-items-active" : ""
+          }`}
+        >
+          <ul>
+            <li>
+              <a href="/">How it works</a>
+            </li>
+            <li>
+              <a href="#">Explore our solutions</a>
+            </li>
+            <li>
+              <a href="/backed-by-research">Backed by research</a>
+            </li>
+            <li>
+              <a href="/about-us">About Us</a>
+            </li>
+            <li className="book-a-demo-mobile-li">
+              <a href="/about-us" className="book-a-demo-mobile">
+                Book a demo
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
