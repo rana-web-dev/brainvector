ffc5ddd8cf082498cecbee6b2f6b586e528d878d
Rana <rana78085@gmail.comm>
2025-10-06 10:05:31 +0600

    add header bg black

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,74 +22,74 @@ const Navigation: React.FC = () => {
   }, []);
 
   return (
-    <nav
-      style={{ width: "100%" }}
-      className={`navigations ${
-        activeMenu || isScrolled ? "bg-black" : "transparent-nav"
-      }`}
-    >
-      <div className="navigations-content">
-        <ul className="menu-for-desktop">
-          <li>
-            <a href="/">
-              <img src="/images/brainvector_logo.svg" alt="Brainvector logo" />
-            </a>
-          </li>
-          <li>
-            <a href="/">How it works</a>
-          </li>
-          <li>
-            <a href="#">Explore our solutions</a>
-          </li>
-          <li>
-            <a href="/backed-by-research">Backed by research</a>
-          </li>
-          <li>
-            <a href="/about-us">About Us</a>
-          </li>
-        </ul>
-        <a href="#" className="book-a-demo">
-          Book a demo
-        </a>
-        <div className="menu-for-mobile">
-          <div className="pe-auto">
-            <a href="/">
-              <img src="/images/brainvector_logo.svg" alt="Brainvector logo" />
-            </a>
-            <button onClick={mobileMenuToggle} aria-label="Toggle mobile menu">
-              {activeMenu ? (
-                <img className="bars" src="/images/close.svg" alt="close" />
-              ) : (
-                <img className="bars" src="/images/bars.svg" alt="bars" />
-              )}
-            </button>
-          </div>
+    <nav className="navigations">
+      <div className={`${activeMenu || isScrolled ? "bg-black" : ""}`}>
+        <div className="navigations-content">
+          <ul className="menu-for-desktop">
+            <li>
+              <a href="/">
+                <img
+                  src="/images/brainvector_logo.svg"
+                  alt="Brainvector logo"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="/">How it works</a>
+            </li>
+            <li>
+              <a href="#">Explore our solutions</a>
+            </li>
+            <li>
+              <a href="/backed-by-research">Backed by research</a>
+            </li>
+            <li>
+              <a href="/about-us">About Us</a>
+            </li>
+          </ul>
+          <a href="#" className="book-a-demo">
+            Book a demo
+          </a>
+        </div>
+      </div>
+      <div className={`menu-for-mobile ${activeMenu ? "bg-black" : ""}`}>
+        <div className={`pe-auto mobile-nav-container ${isScrolled ? "bg-black" : ""}`}>
+          <a href="/">
+            <img src="/images/brainvector_logo.svg" alt="Brainvector logo" />
+          </a>
+          <button onClick={mobileMenuToggle} aria-label="Toggle mobile menu">
+            {activeMenu ? (
+              <img className="bars" src="/images/close.svg" alt="close" />
+            ) : (
+              <img className="bars" src="/images/bars.svg" alt="bars" />
+            )}
+          </button>
+        </div>
 
-          <div
-            className={`mobile-menu-items ${
-              activeMenu ? "mobile-menu-items-active" : ""
-            }`}
-          >
-            <ul>
-              <li>
-                <a href="/">How it works</a>
-              </li>
-              <li>
-                <a href="#">Explore our solutions</a>
-              </li>
-              <li>
-                <a href="/backed-by-research">Backed by research</a>
-              </li>
-              <li>
-                <a href="/about-us">About Us</a>
-              </li>
-              <li className="book-a-demo-mobile-li">
-                <a href="/about-us" className="book-a-demo-mobile">
-                  Book a demo
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div
+          className={`mobile-menu-items ${
+            activeMenu ? "mobile-menu-items-active" : ""
+          }`}
+        >
+          <ul>
+            <li>
+              <a href="/">How it works</a>
+            </li>
+            <li>
+              <a href="#">Explore our solutions</a>
+            </li>
+            <li>
+              <a href="/backed-by-research">Backed by research</a>
+            </li>
+            <li>
+              <a href="/about-us">About Us</a>
+            </li>
+            <li className="book-a-demo-mobile-li">
+              <a href="/about-us" className="book-a-demo-mobile">
+                Book a demo
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
